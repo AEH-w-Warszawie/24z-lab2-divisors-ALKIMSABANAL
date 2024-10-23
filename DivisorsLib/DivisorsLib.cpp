17ece2d9a84c90513c01cce4c63020a0b8d355fb
UgurReyhan <163755746+UgurReyhan@users.noreply.github.com>
2024-10-23 19:04:17 +0300

    Update DivisorsLib.cpp
    Optimized divisors function implementation

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,16 @@
 #include <vector>
-#include "Divisors.h"
+#include <algorithm> 
 
-std::vector<unsigned long long> divisors(unsigned long long number)
-{
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+std::vector<unsigned long long> divisors(unsigned long long number) {
+    std::vector<unsigned long long> result;
+    for (unsigned long long i = 1; i * i <= number; ++i) {
+        if (number % i == 0) {
+            result.push_back(i);
+            if (i != number / i) { 
+                result.push_back(number / i);
+            }
+        }
+    }
+    std::sort(result.begin(), result.end()); 
+    return result;
 }
